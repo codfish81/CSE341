23e34c54ca005c40234245ba44d118c79df012be
codfish81 <codfish81@byui.edu>
2023-05-02 20:03:31 -0600

    added start function

--- db/connect.js
+++ db/connect.js
@@ -19,6 +19,14 @@ const initDb = (callback) => {
     });
 };
 
+async function start(){
+  await client.connect()
+  console.log("Connected")
+  module.exports = client.db()
+  const app = require('./app')
+  app.listen(3000)
+};
+
 const getDb = () => {
   if (!_db) {
     throw Error('Db not initialized');
@@ -29,4 +37,5 @@ const getDb = () => {
 module.exports = {
   initDb,
   getDb,
+  start
 };
