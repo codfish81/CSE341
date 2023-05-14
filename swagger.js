cee35bc0a7a0803254810cc1babbacb93985e51d
codfish81 <codfish81@byui.edu>
2023-05-13 21:47:29 -0600

    changed host

--- swagger.js
+++ swagger.js
@@ -5,7 +5,8 @@ const doc = {
     title: 'My API',
     description: 'Contacts API',
   },
-  host: 'localhost:8080',
+  //host: 'localhost:8080',
+  host: 'https://week2-26b1.onrender.com',
   schemes: ['http'],
 };
 
